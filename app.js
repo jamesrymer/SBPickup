cda8d561b73bb82f6879958cd278519075153530
James Rymer <rymerjamess@gmail.com>
2017-07-25 23:17:00 +0000

    Add routes for new, and create for parks

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 var express             = require("express");
 var app                 = express();
 var bodyParser          = require("body-parser");
-//var request             = require("request");
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+var parks = [
+    {name: "twin creeks" },    
+    {name: "random1" },   
+    {name: "random 2" },   
+    {name: "random3" }    
+];
+    
+// ================================
+// PARKS ROUTES
+// ================================
+
+//INDEX Displays All Parks
 app.get("/parks", function(req, res){
-    res.render("parks.ejs");
+    res.render("parks.ejs",{parks: parks});
+});
+
+app.get("/parks/new", function(req, res){
+    res.render("new.ejs");
+});
+
+app.post("/parks", function(req, res){
+  var park = req.body.park;
+  
+  parks.push(park);
+  res.redirect("/parks");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
