86c395994768314d557ed132a5b45777c9c1f0cf
James Rymer <rymerjamess@gmail.com>
2017-07-26 01:19:09 +0000

    set up and connect mongodb

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express             = require("express");
 var app                 = express();
 var bodyParser          = require("body-parser");
+var mongoose            = require("mongoose");
 
+mongoose.connect("mongodb://localhost/sbpickup");
 app.use(bodyParser.urlencoded({extended: true}));
 
 var parks = [
