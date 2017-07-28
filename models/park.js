755d0b98b5d663614844a070f5261aab27280da3
James Rymer <rymerjamess@gmail.com>
2017-07-28 02:00:52 +0000

    Add local authentication. Refactor project

--- models/park.js
+++ models/park.js
@@ -1,15 +1,15 @@
 var mongoose = require("mongoose");
 
-// Creates the park schema
 var parkSchema = new mongoose.Schema({
-    name: String,
-    games: [
+   name: String,
+   image: String,
+   description: String,
+   games: [
       {
          type: mongoose.Schema.Types.ObjectId,
          ref: "Game"
       }
    ]
 });
-
 
 module.exports = mongoose.model("Park", parkSchema);
